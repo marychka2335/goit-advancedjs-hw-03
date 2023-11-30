eace3edd150d56539c156d983a545ca1a12c85e9
marychka2335 <marychka2335@icloud.com>
2023-12-01 00:28:51 +0200

    third commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,14 +1,50 @@
 import axios from 'axios';
 const BASE_URL = 'https://api.thecatapi.com/v1';
-axios.defaults.headers.common['x-api-key'] =
-  'live_SWwNDJ6lixvIgMTFSA1O4wT3iaw1WygGWikOVhdwUP1RCHZe5g4W3gX9Bf2NR5Jv';
 
-export function fetchBreeds() {
+// function fetchBreeds(url) {
+//   return fetch(url).then(response => response.json());
+// }
+ function fetchBreeds() {
   return axios.get(`${BASE_URL}/breeds`).then(response => response.data);
 }
 
-export function fetchCatByBreed(breedId) {
-  return axios
-    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-    .then(response => response.data);
+function fetchCatByBreed(breedId) {
+  axios.defaults.headers.common['x-api-key'] =
+  'live_SWwNDJ6lixvIgMTFSA1O4wT3iaw1WygGWikOVhdwUP1RCHZe5g4W3gX9Bf2NR5Jv';
+const KEY_API = axios.defaults.headers.common['x-api-key'];
+  // const KEY_API =
+  //   'live_SWwNDJ6lixvIgMTFSA1O4wT3iaw1WygGWikOVhdwUP1RCHZe5g4W3gX9Bf2NR5Jv';
+  const urlСatByBreed = `https://api.thecatapi.com/v1/images/search?api_key=${KEY_API}&limit=1&breed_ids=${breedId}`;
+
+  return fetch(urlСatByBreed).then(response => response.json());
 }
+
+export { fetchBreeds, fetchCatByBreed };
+
+
+// const BASE_URL = 'https://api.thecatapi.com/v1';
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_SWwNDJ6lixvIgMTFSA1O4wT3iaw1WygGWikOVhdwUP1RCHZe5g4W3gX9Bf2NR5Jv';
+
+// export function fetchBreeds() {
+//   return axios.get(`${BASE_URL}/breeds`).then(response => response.data);
+// }
+
+// export function fetchCatByBreed(breedId) {
+//   return axios
+//     .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+//     .then(response => response.data);
+// }
+
+
+// import Notiflix from 'notiflix';
+// import iziToast from 'izitoast';
+
+// export function fetchBreeds() {
+    
+//     return fetch(`https://api.thecatapi.com/v1/${breeds}/?fields=breed,description,temperament,photos`).then(response => {
+//         if (!response.ok) {
+//             iziToast.warning({message: 'Oops, there is no country with that name.'})
+//         } return response.json()
+//     }).catch(error => console.log(error))
+// }
--- src/index.js
+++ src/index.js
@@ -1,62 +1,229 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-import iziToast from 'izitoast';
 import SlimSelect from 'slim-select';
-import 'izitoast/dist/css/iziToast.min.css';
 
-const breedSelect = document.querySelector('.breed-select');
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import iziToast from 'izitoast';
+
+new SlimSelect({
+  select: '#selectElement',
+});
+
+const breedList = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 
-catInfo.classList.add('is-hidden');
+const url = `https://api.thecatapi.com/v1/breeds`;
 
-breedSelect.addEventListener('change', createMarkup);
-
-updateSelect();
-
-function updateSelect(data) {
-  loader.classList.remove('is-hidden');
-
-  fetchBreeds(data)
-    .then(data => {
-      loader.classList.add('is-hidden');
-      breedSelect.classList.remove('is-hidden');
-      breedSelect.innerHTML = '';
-      let markSelect = data.map(({ name, id }) => {
-        return `<option value ='${id}'>${name}</option>`;
-      });
-      breedSelect.insertAdjacentHTML('beforeend', markSelect);
-      new SlimSelect({
-        select: breedSelect,
-      });
+// вызов функции при загрузке страницы
+document.addEventListener('DOMContentLoaded', () => {
+  fetchBreeds(url)
+    .then(json => {
+      // делаем проверку получения данных
+      // если нет данных - тогда ошибка
+      if ('message' in json) {
+        loader.classList.add('visually-hidden');
+        throw new Error();
+      }
+      //если данные есть - рендерим разметку
+      breedList.classList.remove('visually-hidden');
+      loader.classList.add('visually-hidden');
+      renderBreedsList(json);
     })
-    .catch(onError);
+    .catch(error => onError(error));
+});
+
+// функция создания разметки при перезагрузке страницы
+function renderBreedsList(breeds) {
+  const markup = breeds
+    .map(({ id, name }) => {
+      return `
+      <option value="${id}">${name}</option>
+      `;
+    })
+    .join('');
+  breedList.innerHTML = markup;
 }
 
-function createMarkup(event) {
-  loader.classList.remove('is-hidden');
-  catInfo.classList.add('is-hidden');
+// получаем выбранное значение породы
+function getSelectBreed(e) {
+  const breedId = e.target.value;
 
-  const breedId = event.currentTarget.value;
+  loader.classList.remove('visually-hidden');
+  catInfo.classList.add('visually-hidden');
 
   fetchCatByBreed(breedId)
-    .then(data => {
-      loader.classList.add('is-hidden');
-      breedSelect.classList.remove('is-hidden');
-      const { url, breeds } = data[0];
-
-      catInfo.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400"/><div class="box"><h2>${breeds[0].name}</h2><p>${breeds[0].description}</p><p><strong>Temperament:</strong> ${breeds[0].temperament}</p></div>`;
-      catInfo.classList.remove('is-hidden');
+    .then(json => {
+      if (json.length === 0) {
+        // делаем проверку данных
+        loader.classList.add('visually-hidden');
+        throw new Error();
+      }
+      //если данные есть - рендерим разметку
+      loader.classList.add('visually-hidden');
+      renderCatInfo(json);
+      catInfo.classList.remove('visually-hidden');
     })
-    .catch(onError);
+    .catch(error => onError(error));
 }
 
-function onError() {
-  loader.classList.add('is-hidden');
-  breedSelect.innerHTML = '';
-
-  iziToast({
-    message: 'Something went wrong! Try reloading the page!',
-    position: "center"
+// функция создания разметки для информации о породе
+function renderCatInfo(breedData) {
+  // добавляем url
+  const markupUrl = breedData.map(({ url }) => {
+    return `
+      <img src="${url}" width="500">              
+      `;
   });
+
+  // вытягиваем и добавляем описание кота
+  const markupName = breedData
+    .flatMap(elem => elem.breeds)
+    .map(({ name, description, temperament }) => {
+      return `
+      <div class="info-container">
+        <h2>${name}</h2>
+        <p>${description}</p>  
+        <p><span style="font-size:120%; font-weight:bold">Temperament:</span> ${temperament}.</p>
+      </div>              
+      `;
+    });
+
+  // вставляем разметку в код
+  catInfo.innerHTML = markupUrl + markupName;
 }
+
+// функция вывода ошибки
+function onError(err) {
+  error.classList.remove('visually-hidden');
+  iziToast.warning({message: 
+    'Oops! Something went wrong! Try reloading the page!'}
+  );
+}
+
+breedList.addEventListener('change', getSelectBreed);
+
+
+// // import { fetchBreeds, fetchCatByBreed } from './cat-api';
+// import SlimSelect from 'slim-select';
+
+
+// import './styles.css';
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
+// // import debounce from 'lodash.debounce';
+// import { fetchBreeds } from './cat-api';
+// import renderingMarkup from './renderingMarkup';
+
+// // const DEBOUNCE_DELAY = 300;
+// const input = document.querySelector('#search-box');
+// const breedSelect = document.querySelector('.breed-select');
+// const catInfo = document.querySelector('.cat-info');
+// const loader = document.querySelector('.loader');
+// const error = document.querySelector('.error');
+
+// // input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+// catInfo.classList.add('is-hidden');
+
+// breedSelect.addEventListener('change', createMarkup);
+
+// function createMarkup(event) {
+//   loader.classList.remove('is-hidden');
+//   catInfo.classList.add('is-hidden');
+//   const breedId = event.currentTarget.value;
+
+//   fetchCatByBreed(breedId)
+//     .then(data => {
+//       loader.classList.add('is-hidden');
+//       breedSelect.classList.remove('is-hidden');
+//       const { url, breeds } = data[0];
+
+//       catInfo.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400"/><div class="box"><h2>${breeds[0].name}</h2><p>${breeds[0].description}</p><p><strong>Temperament:</strong> ${breeds[0].temperament}</p></div>`;
+//       catInfo.classList.remove('is-hidden');
+//     })
+//     .catch(onError);
+// }
+// updateSelect();
+
+// function updateSelect(data) {
+//   loader.classList.remove('is-hidden');
+
+//   fetchBreeds(data)
+//     .then(data => {
+//       loader.classList.add('is-hidden');
+//       breedSelect.classList.remove('is-hidden');
+//       breedSelect.innerHTML = '';
+//       let markSelect = data.map(({ name, id }) => {
+//         return `<option value ='${id}'>${name}</option>`;
+//       });
+//       breedSelect.insertAdjacentHTML('beforeend', markSelect);
+//       new SlimSelect({
+//         select: breedSelect,
+//       });
+//     })
+//     .catch(onError);
+// }
+
+
+// function onSearch (evt){
+//     const searchQuery = evt.target.value.trim();
+//     if (searchQuery !== '') {
+//         fetchBreeds(searchQuery).then(renderingMarkup).catch(error => console.log(error));
+//     }
+// };
+
+
+
+// catInfo.classList.add('is-hidden');
+
+// breedSelect.addEventListener('change', createMarkup);
+
+// updateSelect();
+
+// function updateSelect(data) {
+//   loader.classList.remove('is-hidden');
+
+//   fetchBreeds(data)
+//     .then(data => {
+//       loader.classList.add('is-hidden');
+//       breedSelect.classList.remove('is-hidden');
+//       breedSelect.innerHTML = '';
+//       let markSelect = data.map(({ name, id }) => {
+//         return `<option value ='${id}'>${name}</option>`;
+//       });
+//       breedSelect.insertAdjacentHTML('beforeend', markSelect);
+//       new SlimSelect({
+//         select: breedSelect,
+//       });
+//     })
+//     .catch(onError);
+// }
+
+// function createMarkup(event) {
+//   loader.classList.remove('is-hidden');
+//   catInfo.classList.add('is-hidden');
+
+//   const breedId = event.currentTarget.value;
+
+//   fetchCatByBreed(breedId)
+//     .then(data => {
+//       loader.classList.add('is-hidden');
+//       breedSelect.classList.remove('is-hidden');
+//       const { url, breeds } = data[0];
+
+//       catInfo.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400"/><div class="box"><h2>${breeds[0].name}</h2><p>${breeds[0].description}</p><p><strong>Temperament:</strong> ${breeds[0].temperament}</p></div>`;
+//       catInfo.classList.remove('is-hidden');
+//     })
+//     .catch(onError);
+// }
+
+// function onError() {
+//   loader.classList.add('is-hidden');
+//   breedSelect.innerHTML = '';
+
+//   iziToast.show({
+//     message: 'Something went wrong! Try reloading the page!',
+//     position: 'TopCenter',
+//     color: red,
+//   });
+// }
